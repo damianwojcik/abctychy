ccb32de683eea6111adab47bebabc528f7e93fa7
damianwojcik <khamian@gmail.com>
2021-04-28 16:40:46 +0200

    rename wp-admin posts to Oferty

--- incl/general.php
+++ incl/general.php
@@ -22,3 +22,31 @@
     wp_enqueue_script( 'lightbox', THEME_URL . '/assets/js/lightbox.min.js', '', NULL);
 		wp_enqueue_script( 'site_scripts', THEME_URL . '/scripts.min.js', '', NULL);
 	}
+
+	function change_post_menu_label() {
+    global $menu, $submenu;
+
+    $menu[5][0] = 'Oferty';
+    $submenu['edit.php'][5][0] = 'Oferty';
+    $submenu['edit.php'][10][0] = 'Nowa Oferta';
+    $submenu['edit.php'][16][0] = 'Oferty tagi';
+    echo '';
+}
+add_action( 'admin_menu', 'change_post_menu_label' );
+
+function change_post_object_label() {
+    global $wp_post_types;
+
+    $labels = &$wp_post_types['post']->labels;
+    $labels->name = 'Oferty';
+    $labels->singular_name = 'Oferta';
+    $labels->add_new = 'Nowa Oferta';
+    $labels->add_new_item = 'Nowa Oferta';
+    $labels->edit_item = 'Edytuj Ofertę';
+    $labels->new_item = 'Nowa Oferta';
+    $labels->view_item = 'Zobacz Ofertę';
+    $labels->search_items = 'Szukaj Oferty';
+    $labels->not_found = 'Nie znaleziono Oferty';
+    $labels->not_found_in_trash = 'Oferty nie ma w koszu';
+}
+add_action( 'init', 'change_post_object_label' );
